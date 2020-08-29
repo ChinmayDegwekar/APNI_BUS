595c42a9ae0a553402db7d138125637a83fcf6ee
ChinmayDegwekar <csdegwekar@gmail.com>
2020-08-29 19:54:21 +0530

    Route finder activity done

--- app/screens/BusRouteScreen.js
+++ app/screens/BusRouteScreen.js
@@ -13,22 +13,79 @@ import Timeline from "react-native-timeline-flatlist";
 class BusRouteScreen extends Component {
   constructor() {
     super();
-    this.data = [
-      { time: "", title: "Event 1", description: "Event 1 Description" },
-      { time: "", title: "Event 2", description: "Event 2 Description" },
-      { time: "", title: "Event 3", description: "Event 3 Description" },
-      { time: "", title: "Event 4", description: "Event 4 Description" },
-      { time: "", title: "Event 5", description: "Event 5 Description" },
-      { time: "", title: "Event 1", description: "Event 1 Description" },
-      { time: "", title: "Event 2", description: "Event 2 Description" },
-      { time: "", title: "Event 3", description: "Event 3 Description" },
-      { time: "", title: "Event 4", description: "Event 4 Description" },
-      { time: "", title: "Event 5", description: "Event 5 Description" },
-    ];
+
+    this.state = {
+      data: 1,
+      isLoading: true,
+    };
+
+    // this.data = [
+    //   { time: "", title: "Event 1", description: "Event 1 Description" },
+    //   { time: "", title: "Event 2", description: "Event 2 Description" },
+    //   { time: "", title: "Event 3", description: "Event 3 Description" },
+    //   { time: "", title: "Event 4", description: "Event 4 Description" },
+    //   { time: "", title: "Event 5", description: "Event 5 Description" },
+    //   { time: "", title: "Event 1", description: "Event 1 Description" },
+    //   { time: "", title: "Event 2", description: "Event 2 Description" },
+    //   { time: "", title: "Event 3", description: "Event 3 Description" },
+    //   { time: "", title: "Event 4", description: "Event 4 Description" },
+    //   { time: "", title: "Event 5", description: "Event 5 Description" },
+    // ];
   }
+  generateRequiredFormat(json) {
+    var result = [];
+    var n = json["route"].length;
+    for (var i = 0; i < n; i++) {
+      var temp = {
+        time: "",
+        title: json["route"][i].bus_stop_name,
+        description: "Stop no. " + (i + 1),
+      };
+      result.push(temp);
+    }
+    return result;
+  }
+
+  getRemoteData = () => {
+    // var src_id = this.props.route.params["src_id"];
+    // var dest_id = this.props.route.params["dest_id"];
+    // console.log(this.props.route.params["source"]);
+    var bus_no = this.props.route.params["bus_num"];
+    var src_id = 106;
+    var dest_id = 336;
+    const url = "http://157.245.110.40/getBusRoute.php/?bus_no=" + bus_no;
+    console.log(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        this.setState({
+          data: this.generateRequiredFormat(json),
+          isLoading: false,
+        });
+      })
+      .catch((error) => {
+        console.log("get data error:" + error);
+      });
+  };
+
   render() {
-    return <Timeline data={this.data} />;
+    this.getRemoteData();
+    console.log(this.state.data);
+    return (
+      <SafeAreaView style={styles.container}>
+        {this.state.isLoading == false ? (
+          <Timeline data={this.state.data} />
+        ) : null}
+      </SafeAreaView>
+    );
   }
 }
 
 export default BusRouteScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
--- app/screens/RouteFinderScreen.js
+++ app/screens/RouteFinderScreen.js
@@ -37,7 +37,7 @@ class RouteFinderScreen extends Component {
               placeholder={"Bus Number"}
               onSelect={(value) => {
                 console.log("value", value);
-                this.state.bus_num = value;
+                this.state.bus_num = value.bus_number;
               }}
               maxHeight={150}
             />
